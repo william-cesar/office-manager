9763d5ca760390e4af0136228de5e907bc0f498e
william-cesar <william.cesar.mg@hotmail.com>
2020-07-14 11:18:15 -0300

    Users models creation

---
+++ office-manager-backend/src/models/users.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define('User', {
+		first_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		last_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		registration_doc: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		birthdate: {
+			type: DataTypes.DATEONLY,
+			allowNull: true,
+		},
+		gender: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		isActive: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: true,
+		},
+	})
+
+	User.associate = (models) => {
+		User.belongsTo(models.Position, { foreignKey: 'positionId' })
+	}
+
+	return User
+}
