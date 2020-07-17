819cfadd2095f33dd4b1aefff1b7d0ff33ee24f2
william-cesar <william.cesar.mg@hotmail.com>
2020-07-17 07:42:56 -0300

    Updated positionsRoutes

--- office-manager-backend/src/routes/positionsRoutes.js
+++ office-manager-backend/src/routes/positionsRoutes.js
@@ -1,18 +1,11 @@
 const express = require('express')
-const { Position, Sequelize } = require('../models')
-const pagination = require('../helpers/pagination')
-const fixInputName = require('../helpers/fixInputName')
+const { Position, Profile, User, Sequelize } = require('../models')
+const paginate = require('../helpers/paginate')
+const standardString = require('../helpers/standardString')
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-	const position = await Position.findAndCountAll({
-		order: [['position_name', 'ASC']],
-	})
-	return res.jsonOK(position)
-})
-
-router.get('/:page', async (req, res) => {
+router.get('/active/:page', async (req, res) => {
 	const { position_name } = req.body
 	const { page } = req.params
 	const limit = 5
@@ -20,12 +13,37 @@ router.get('/:page', async (req, res) => {
 	const position = await Position.findAndCountAll({
 		where: {
 			position_name: {
-				[Sequelize.Op.iLike]: `%${fixInputName(position_name)}%`,
+				[Sequelize.Op.iLike]: `%${standardString(position_name)}%`,
 			},
+			isActive: true,
 		},
 		order: [['position_name', 'ASC']],
 		limit,
-		offset: pagination(page),
+		offset: paginate(page),
+	})
+
+	if (position.count != 0) {
+		return res.jsonOK(position)
+	} else {
+		return res.jsonNotFound(null)
+	}
+})
+
+router.get('/inactive/:page', async (req, res) => {
+	const { position_name } = req.body
+	const { page } = req.params
+	const limit = 5
+
+	const position = await Position.findAndCountAll({
+		where: {
+			position_name: {
+				[Sequelize.Op.iLike]: `%${standardString(position_name)}%`,
+			},
+			isActive: false,
+		},
+		order: [['position_name', 'ASC']],
+		limit,
+		offset: paginate(page),
 	})
 
 	if (position.count != 0) {
@@ -39,33 +57,49 @@ router.post('/', async (req, res) => {
 	const { position_name } = req.body
 	const { profileId } = req.body
 
-	if (!fixInputName(position_name)) {
+	if (!standardString(position_name)) {
 		return res.jsonBadRequest(null, 'Position name must not be empty.')
 	}
 
 	const position = await Position.findOne({
-		where: { position_name: fixInputName(position_name) },
+		where: { position_name: standardString(position_name) },
 	})
+
 	if (position) {
 		return res.jsonBadRequest(null, 'Position already exists.')
 	}
 
 	if (!profileId) {
-		return res.jsonBadRequest(null, 'Position must be attached to a Profile.')
+		return res.jsonBadRequest(
+			null,
+			'Position must be attached to an active Profile.'
+		)
+	}
+
+	const profile = await Profile.findOne({
+		where: { id: profileId, isActive: true },
+	})
+
+	if (!profile) {
+		return res.jsonBadRequest(
+			null,
+			'Position must be attached to an active Profile.'
+		)
 	}
 
 	const newPosition = await Position.create({
-		position_name: fixInputName(position_name),
+		position_name: standardString(position_name),
 		profileId,
 	})
 
 	return res.jsonOK(newPosition)
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/edit/:id', async (req, res) => {
 	const { id } = req.params
 	const { position_name } = req.body
 	const { profileId } = req.body
+	const { isActive } = req.body
 
 	const position = await Position.findOne({ where: { id } })
 
@@ -73,16 +107,30 @@ router.put('/:id', async (req, res) => {
 		return res.jsonNotFound(null)
 	}
 
-	if (!fixInputName(position_name)) {
+	if (!standardString(position_name)) {
 		return res.jsonBadRequest(null, 'Position name must not be empty.')
 	}
 
 	if (!profileId) {
-		return res.jsonBadRequest(null, 'Position must be attached to a Profile.')
+		return res.jsonBadRequest(
+			null,
+			'Position must be attached to an active Profile.'
+		)
+	}
+
+	const profile = await Profile.findOne({
+		where: { id: profileId, isActive: true },
+	})
+
+	if (!profile) {
+		return res.jsonBadRequest(
+			null,
+			'Position must be attached to an active Profile.'
+		)
 	}
 
 	const updatePosition = await Position.update(
-		{ position_name: fixInputName(position_name), profileId },
+		{ position_name: standardString(position_name), isActive, profileId },
 		{ where: { id } }
 	)
 	const updatedPosition = await Position.findOne({ where: { id } })
@@ -90,22 +138,38 @@ router.put('/:id', async (req, res) => {
 	return res.jsonOK(updatedPosition)
 })
 
-router.delete('/:id', async (req, res) => {
+router.patch('/:id', async (req, res) => {
 	const { id } = req.params
 
-	const position = await Position.findOne({ where: { id } })
+	const position = await Position.findOne({
+		include: [{ model: User }],
+		where: { id, isActive: true },
+	})
 
 	if (!position) {
 		return res.jsonNotFound(null)
 	}
 
-	const isEmpty = await Position.findAndCountAll({ where: { id } })
+	if (position) {
+		const { Users } = position.dataValues
 
-	if (isEmpty.count != 0) {
-		return res.jsonBadRequest(null, 'Position is not empty')
+		let isNotEmpty = Users.filter((item) => {
+			return item.dataValues.isActive == true
+		})
+
+		console.log(isNotEmpty)
+		if (isNotEmpty != '') {
+			return res.jsonBadRequest(
+				null,
+				'This position has at least one user attached to it.'
+			)
+		}
 	}
 
-	const deletePosition = await Position.destroy({ where: { id } })
+	const setPositionInactive = await Position.update(
+		{ isActive: false },
+		{ where: { id } }
+	)
 	return res.jsonOK(null, 'Position deleted successfully')
 })
 
